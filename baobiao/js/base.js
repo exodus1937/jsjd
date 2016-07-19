400d80600c0291455c3fb096eef311a655e86b09
duleigiser <564779666@qq.com>
2016-07-19 16:57:32 +0800

    添加新的报表

--- baobiao/js/base.js
+++ baobiao/js/base.js
@@ -5,9 +5,10 @@
         window.location.origin = window.location.protocol + "//" + window.location.hostname + (window.location.port ? ':' + window.location.port : '');
     }
 var ctx = window.location.origin;
-var org_id = localStorage.getItem("orgid_baobiao");
+var org_id;
 window.onload = function () {
-    
+    org_id = localStorage.getItem("orgid");
+    console.log(org_id)
     var stree = $(".st_tree");
     stree.SimpleTree({
         click:function(a) {
@@ -30,8 +31,18 @@ window.onload = function () {
     });
    
     $(".st_tree span").each(function(){      
-        var org_name;
-        var _resid=this.id;
+        
+
+         var _resid       ="",
+             general_type ='',
+             org_name     ='',
+             mix          = this.id.split(",");
+        if(mix.length==2){
+            _resid=mix[0];    
+           general_type=mix[1]
+        }else{
+             _resid=mix[0]; 
+        }
         
         if(org_id == 'a61365e2-969d-4352-b3f8-805027ab9f1d'|| org_id == "c21834b4-1cb0-490f-a2a8-deeaf7f7e065"){
             org_id   = "c21834b4-1cb0-490f-a2a8-deeaf7f7e065";
@@ -39,7 +50,7 @@ window.onload = function () {
         }else if(org_id == "472212af-1977-462b-a74a-a1f36ed6562d"){
             org_name = encodeURI("宁东发电")
         }
-        var _this_url = ctx+"/spreadsheet/vision/openresource.jsp?paramsInfo=[{'name':'org_id','value':'"+org_id+"','displayValue':'"+org_name+"'}]&resid="+_resid +"&id=displaycatalog&catalogid=I8a8a8aa0308027e30152339cf073025a&showbanner=false&user=liulan&password=liulan";
+        var _this_url = ctx+"/spreadsheet/vision/openresource.jsp?paramsInfo=[{'name':'org_id','value':'"+org_id+"','displayValue':'"+org_name+"'},{'name':'general_type','value':'"+general_type+"'}]&resid="+_resid +"&id=displaycatalog&catalogid=I8a8a8aa0308027e30152339cf073025a&showbanner=false&user=liulan&password=liulan";
          
 
         $(this).click(function(){
